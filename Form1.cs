85ce7f704fe71b9ab1a4ecec9a9e84f912564293
nhat123113 <Aqtvmsdw@gmail.com>
2017-04-26 07:29:18 +0700

    GitThem

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QLTV.view;
+namespace QLTV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FormSACH form = new FormSACH();
+            form.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FormKHACH form = new FormKHACH();
+            form.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FormPM form = new FormPM();
+            form.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FormTM form = new FormTM();
+            form.Show();
+        }
+    }
+}
